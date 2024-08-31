c8db2d45d4a653be7e3a9cf2d5770e9d84df111b
ELbekG <twobealfa@yandex.ru>
2024-08-31 14:49:54 +0500

    added migration files

---
+++ schema/000002_init.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS product
+(
+    id BIGSERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    price NUMERIC(6, 2) NOT NULL,
+    quantity INT NOT NULL
+);
---
+++ schema/000003_init.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO product (id, name, description, price, quantity)
+VALUES (1, 'Rayan Gosling', 'Drive', 123, 12);
